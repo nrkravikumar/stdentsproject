004af28bc8530f4412f203dd378c4f1f28bed7fb
N. Ravi Kumar[NRK] <rravikumar34@gmail.com>
2025-12-06 15:30:34 +0530

    Update settings.py

--- MainProject/settings.py
+++ MainProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qm3=k595actsx*wwnu_zgh4$*ze^u0-7dj4e(-n76&oz(fxt%&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
